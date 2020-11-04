e943ba530ac5db763735a8332b85168cbdf32837
cadena87 <cadena87@hotmail.com>
2020-11-04 19:01:06 -0300

    deploy

--- src/main/java/com/example/tasklist/TasklistApplication.java
+++ src/main/java/com/example/tasklist/TasklistApplication.java
@@ -2,10 +2,8 @@ package com.example.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = "com.example.tasklist.config")
 public class TasklistApplication {
 
     public static void main(String[] args) {
