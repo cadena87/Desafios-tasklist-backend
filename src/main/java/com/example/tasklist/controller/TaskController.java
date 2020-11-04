c1cdcb968d470e11d871fe758885d607532ec5bb
cadena87 <cadena87@hotmail.com>
2020-11-04 14:05:49 -0300

    deploy

--- src/main/java/com/example/tasklist/controller/TaskController.java
+++ src/main/java/com/example/tasklist/controller/TaskController.java
@@ -24,7 +24,7 @@ import java.util.Locale;
 @Slf4j
 @RequiredArgsConstructor
 @Api(value = "Task", description = "Api de gerenciamento de Tarefas")
-@CrossOrigin
+@CrossOrigin(origins = "https://app-tasklist-supero.herokuapp.com")
 public class TaskController {
 
     @Autowired
