dc29f9ad4861d29674ea1197ebc4f8db96445730
cadena87 <cadena87@hotmail.com>
2020-11-04 13:24:18 -0300

    deploy

--- src/main/java/com/example/tasklist/config/WebSecurityConfig.java
+++ src/main/java/com/example/tasklist/config/WebSecurityConfig.java
@@ -2,22 +2,24 @@ package com.example.tasklist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD","OPTIONS")
-                        .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept");
-            }
-        };
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://app-tasklist-supero.herokuapp.com");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("GET");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
