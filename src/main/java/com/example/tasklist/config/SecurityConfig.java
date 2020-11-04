d615f9c3f9dd28949c5e2d8f37ab59227a0e0e5a
cadena87 <cadena87@hotmail.com>
2020-11-04 15:26:08 -0300

    deploy

--- src/main/java/com/example/tasklist/config/SecurityConfig.java
+++ src/main/java/com/example/tasklist/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig  {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://app-tasklist-supero.herokuapp.com");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
