690a0e7b078341cce02b535510edb05d3df9f99e
cadena87 <cadena87@hotmail.com>
2020-11-04 16:59:08 -0300

    deploy

--- src/main/java/com/example/tasklist/TasklistApplication.java
+++ src/main/java/com/example/tasklist/TasklistApplication.java
@@ -2,8 +2,10 @@ package com.example.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.example.tasklist.config")
 public class TasklistApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/tasklist/config/RestConfig.java
+++ src/main/java/com/example/tasklist/config/RestConfig.java
@@ -9,40 +9,32 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestConfig implements Filter {
 
-    private final Logger log = LoggerFactory.getLogger(RestConfig.class);
-
-    public RestConfig() {
-            log.info("SimpleCORSFilter init");
-    }
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        HttpServletRequest request =  (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "Location");
 
-        chain.doFilter(req, res);
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+
+        } else {
+            chain.doFilter(req, res);
+        }
     }
+    @Override
+    public void init(FilterConfig filterConfig) {}
 
     @Override
-    public void init(FilterConfig filterConfig) {
-    }
-
-    @Override
-    public void destroy() {
-    }
-
+    public void destroy() {}
 }
