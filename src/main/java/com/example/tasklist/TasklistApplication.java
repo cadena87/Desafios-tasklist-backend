5e5b177a6299b02b2b81984687f020eb7c66b3b6
cadena87 <cadena87@hotmail.com>
2020-11-04 16:45:34 -0300

    deploy

--- src/main/java/com/example/tasklist/TasklistApplication.java
+++ src/main/java/com/example/tasklist/TasklistApplication.java
@@ -2,32 +2,11 @@ package com.example.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 public class TasklistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TasklistApplication.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("https://ws-tasklist-api.herokuapp.com/"));
-        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Access-Control-Allow-Origin"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-        source.registerCorsConfiguration("/api/v1/**", config);
-        FilterRegistrationBean registration = new FilterRegistrationBean(new CorsFilter(source));
-        return registration;
     }
 }
