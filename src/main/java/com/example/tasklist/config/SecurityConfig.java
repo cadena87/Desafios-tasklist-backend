d11f277d7339fc5ca0fcf19e25c31059ec06dfb6
cadena87 <cadena87@hotmail.com>
2020-11-04 14:42:31 -0300

    deploy

--- src/main/java/com/example/tasklist/config/SecurityConfig.java
+++ src/main/java/com/example/tasklist/config/SecurityConfig.java
@@ -10,11 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowCredentials(false);
+        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
     }
 }
