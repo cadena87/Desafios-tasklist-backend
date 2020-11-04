51d3ef760c8d44dc984cb64d630596a25f47970d
cadena87 <cadena87@hotmail.com>
2020-11-04 15:15:08 -0300

    deploy

--- src/main/java/com/example/tasklist/config/SecurityConfig.java
+++ src/main/java/com/example/tasklist/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.example.tasklist.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+public class SecurityConfig  {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://app-tasklist-supero.herokuapp.com");
+            }
+        };
     }
 }
