81c67d8da3719c37bf90c75a97ae9ae375c88711
cadena87 <cadena87@hotmail.com>
2020-11-04 09:56:40 -0300

    deploy

--- src/main/java/com/example/tasklist/config/WebConfig.java
+++ src/main/java/com/example/tasklist/config/WebConfig.java
@@ -1,21 +1,16 @@
 package com.example.tasklist.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://app-tasklist-supero.herokuapp.com");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://app-tasklist-supero.herokuapp.com")
+                .allowedMethods("GET,POST,PUT,DELETE");
     }
 }
