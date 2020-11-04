589ef9aba4ac483bc5b40b1c52ad05a5989d2a98
cadena87 <cadena87@hotmail.com>
2020-11-04 14:31:10 -0300

    deploy

--- src/main/java/com/example/tasklist/config/SecurityConfig.java
+++ src/main/java/com/example/tasklist/config/SecurityConfig.java
@@ -3,34 +3,16 @@ package com.example.tasklist.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(final HttpSecurity http) throws Exception {
         http
             .cors()
-            .configurationSource(new PermissiveCorsConfigurationSource())
             .and()
             .csrf()
             .disable()
             .authorizeRequests()
             .antMatchers("**").permitAll();
-    }
-
-    private static class PermissiveCorsConfigurationSource implements CorsConfigurationSource {
-        @Override
-        public CorsConfiguration getCorsConfiguration(final HttpServletRequest request) {
-            final CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowCredentials(false);
-            configuration.setAllowedHeaders(Collections.singletonList("*"));
-            configuration.setAllowedMethods(Collections.singletonList("*"));
-            configuration.setAllowedOrigins(Collections.singletonList("*"));
-            return configuration;
-        }
     }
 }
