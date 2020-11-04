a7c336ad2b507ee509c06d9918413ba56c4a8491
cadena87 <cadena87@hotmail.com>
2020-11-04 14:37:44 -0300

    deploy

--- src/main/java/com/example/tasklist/config/SecurityConfig.java
+++ src/main/java/com/example/tasklist/config/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.example.tasklist.config;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    public void configure(final HttpSecurity http) throws Exception {
-        http
-            .cors()
-            .and()
-            .csrf()
-            .disable()
-            .authorizeRequests()
-            .antMatchers("**").permitAll();
+@EnableWebMvc
+public class SecurityConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
     }
 }
