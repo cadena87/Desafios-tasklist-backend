7a48ffffee9de94c5e8f2758f18734e50f0ec86d
cadena87 <cadena87@hotmail.com>
2020-11-04 13:59:06 -0300

    deploy

--- src/main/java/com/example/tasklist/controller/TaskController.java
+++ src/main/java/com/example/tasklist/controller/TaskController.java
@@ -32,7 +32,7 @@ public class TaskController {
     @Autowired
     private MessageSource messageSource;
 
-    @CrossOrigin
+    @CrossOrigin(origins = "https://app-tasklist-supero.herokuapp.com")
     @PostMapping(produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Task> create(@RequestBody Task task) {
         try {
