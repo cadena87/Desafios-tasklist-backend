bd9cadb347273989d0ab71c3ff6d71b2d5a13ea2
cadena87 <cadena87@hotmail.com>
2020-11-04 00:35:56 -0300

    deploy

--- src/main/java/com/example/tasklist/config/WebSecurityConfig.java
+++ src/main/java/com/example/tasklist/config/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
     }
 }
