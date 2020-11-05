a9e3c1709adb3977dd09d6e24dc3935cb203d6df
cadena87 <cadena87@hotmail.com>
2020-11-05 17:05:27 -0300

    deploy

--- src/main/java/com/example/tasklist/TasklistApplication.java
+++ src/main/java/com/example/tasklist/TasklistApplication.java
@@ -2,11 +2,27 @@ package com.example.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TasklistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TasklistApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/task/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+
+
+            }
+        };
     }
 }
