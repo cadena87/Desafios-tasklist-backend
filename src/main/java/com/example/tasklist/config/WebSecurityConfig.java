984ea5fa45572f207e24b900989ff0f72dcad5b6
cadena87 <cadena87@hotmail.com>
2020-11-05 16:44:41 -0300

    deploy

--- src/main/java/com/example/tasklist/config/WebSecurityConfig.java
+++ src/main/java/com/example/tasklist/config/WebSecurityConfig.java
@@ -11,7 +11,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+        http.cors()
                 .and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/v1/task").permitAll()
